751793c7316b76a1a1be7a3f3416fcfd8d004359
Kim Medina <ctmedina2@dons.usfca.edu>
2024-10-22 12:39:52 -0700

    Moved code line inside function

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -3,11 +3,11 @@ library(dplyr)
 library(ggplot2)
 library(knitr)
 
-# Read the CSV file into a data frame
-snp_data <- read.csv("data/sample_snp_data.csv")
-
 # First function
 subset_snp_data <- function(dataset, sample_id) {
+
+  # Read the CSV file into a data frame
+  snp_data <- read.csv("data/sample_snp_data.csv")
 
   # Subset the data to include only the specified sample
   subset_data <- snp_data[snp_data$sample == sample_id, ]
