3146d6ab2b22b68f16deac5c9a586815327cbcf4
Kim Medina <ctmedina2@dons.usfca.edu>
2024-11-06 15:03:29 -0800

    Resolved github comments

--- code/functions/subset_data_function.R
+++ code/functions/subset_data_function.R
@@ -2,7 +2,7 @@
 subset_snp_data <- function(dataset, sample_id) {
 
   # Read the CSV file into a data frame
-  snp_data <- read.csv("data/sample_snp_data.csv")
+  snp_data <- read.csv(dataset)
 
   # Subset the data to include only the specified sample
   subset_data <- snp_data[snp_data$sample == sample_id, ]
