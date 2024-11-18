bf5cabec0691f8abb7d3a6eb237e3f311191d031
kmedina21 <ctmedina2@dons.usfca.edu>
2024-11-18 01:27:14 +0000

    Added country count code

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -3,3 +3,5 @@
 count=$(zgrep -c "^>" /blast-db/sars-cov-2-seq-data/2022-10-17_ncbi_sars-cov-2-sequences.fasta.gz)
 echo "The amount of sequences in this file is: $count"
 
+countries=$(zcat /blast-db/sars-cov-2-seq-data/2022-10-17_ncbi_sars-cov-2-sequences.fasta.gz | \awk '/^>/ {split($0, a, "|"); print a[length(a)-5]}' | \sort | \uniq -c | \sort -nr)
+echo "These are the countries in the dataset sorted from largest to smallest: $countries"
