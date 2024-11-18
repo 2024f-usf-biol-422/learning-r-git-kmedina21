fa7da85755dc8c29368bbe1fe960ab926cc849de
kmedina21 <ctmedina2@dons.usfca.edu>
2024-11-18 01:55:37 +0000

    Adjust pathway code

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 count=$(zgrep -c "^>" /blast-db/sars-cov-2-seq-data/2022-10-17_ncbi_sars-cov-2-sequences.fasta.gz)
-echo "The amount of sequences in this file is: $count" > Medina_Kim/learning-r-git-kmedina21/output/parse_result.txt
+echo "The amount of sequences in this file is: $count" > ../output/parse_result.txt
 
 countries=$(zcat /blast-db/sars-cov-2-seq-data/2022-10-17_ncbi_sars-cov-2-sequences.fasta.gz | \awk '/^>/ {split($0, a, "|"); print a[length(a)-5]}' | \sort | \uniq -c | \sort -nr)
-echo "These are the countries in the dataset sorted from largest to smallest: $countries" >> Medina_Kim/learning-r-git-kmedina21/output/parse_result.txt
+echo "These are the countries in the dataset sorted from largest to smallest: $countries" >> ../output/parse_result.txt
